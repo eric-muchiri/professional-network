a907f8ca4a04b888ccf2cde7c980340db0f59c12
eric-muchiri <ericmuchiri02@gmail.com>
2024-06-04 22:20:18 +0300

    dashboard layout

---
+++ app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,19 @@
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+import SessionProvider from "@/components/session-provider";
+import AppShellContainer from "@/components/app-shell-container";
+
+export default async function Layout({ children }: { children: ReactNode }) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/signin");
+  }
+  return (
+    <SessionProvider session={session}>
+      <AppShellContainer>{children}</AppShellContainer>
+    </SessionProvider>
+  );
+}
