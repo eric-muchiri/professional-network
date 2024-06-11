07ffe38c0e6fd37ae99e8401d2c1c1de7386c9d5
eric-muchiri <ericmuchiri02@gmail.com>
2024-06-11 13:15:07 +0300

    header menu

--- components/app-shell-container.tsx
+++ components/app-shell-container.tsx
@@ -1,6 +1,13 @@
 "use client";
-import { AppShell } from "@mantine/core";
+import { AppShell, Avatar, Burger, Menu } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import {
+  IconDoorExit,
+  IconSettings,
+  IconUserCircle,
+} from "@tabler/icons-react";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
@@ -18,7 +25,41 @@ export default function AppShellContainer({
       navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: !opened } }}
       padding="md"
     >
-      <AppShell.Header>Header</AppShell.Header>
+      <AppShell.Header>
+        <div className="flex justify-between h-full">
+          {" "}
+          <div className="flex h-full items-center gap-5 p-5">
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
+            />
+            Professional Network
+          </div>
+          <div className="flex justify-end gap-5 h-full items-center p-5">
+            <div>search</div>
+            <div>
+              <Menu>
+                <Menu.Target>
+                  <Avatar>
+                    <IconUserCircle />
+                  </Avatar>
+                </Menu.Target>
+                <Menu.Dropdown>
+                  <Menu.Label>Menu</Menu.Label>
+                  <Menu.Item leftSection={<IconSettings />}>
+                    <Link href="/dashboard/profile">Edit profile</Link>
+                  </Menu.Item>
+                  <Menu.Item leftSection={<IconDoorExit />}>
+                    <button onClick={() => signOut()}>Sign out</button>
+                  </Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            </div>
+          </div>
+        </div>
+      </AppShell.Header>
       <AppShell.Navbar>Navbar</AppShell.Navbar>
       <AppShell.Main>{children}</AppShell.Main>
     </AppShell>
