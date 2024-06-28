57a67784a4d8336596f746f60748dce8737bf916
eric-muchiri <ericmuchiri02@gmail.com>
2024-06-28 21:49:46 +0300

    pagination

---
+++ components/pagignation-container.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Pagination } from "@mantine/core";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+export default function PaginationContainer({ total }: { total: number }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleChange(page: number) {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    replace(`${pathname}?${params.toString()}`);
+  }
+  return <Pagination total={total} onChange={handleChange} />;
+}
