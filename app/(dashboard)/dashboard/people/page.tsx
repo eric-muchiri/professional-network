66b0f7bb76f04d18ec6b5acd5048edda20ee3781
eric-muchiri <ericmuchiri02@gmail.com>
2024-06-28 21:50:28 +0300

    paginate user list

---
+++ app/(dashboard)/dashboard/people/page.tsx
@@ -0,0 +1,42 @@
+import PaginationContainer from "@/components/pagignation-container";
+import { db } from "@/lib/db";
+import { users } from "@/lib/schema";
+import { count } from "drizzle-orm";
+
+const PER_PAGE = 20;
+
+async function getUsers(page: number) {
+  const countRes = await db.select({ value: count() }).from(users);
+  const offset = PER_PAGE * (page - 1);
+  const data = await db.query.users.findMany({
+    limit: PER_PAGE,
+    offset: offset,
+  });
+  const userCount = countRes[0].value;
+  const numPages = Math.ceil(userCount / PER_PAGE);
+  return {
+    data: data,
+    count: userCount,
+    numPages: numPages,
+  };
+}
+
+interface Props {
+  searchParams: { page: number };
+}
+
+export default async function Page({ searchParams }: Props) {
+  const page = searchParams.page || 1;
+  const res = await getUsers(page);
+
+  return (
+    <div>
+      {res.data.map((user) => (
+        <div key={user.id}>
+          {user.name}-{user.jobTitle}
+        </div>
+      ))}
+      <PaginationContainer total={res.numPages} />
+    </div>
+  );
+}
